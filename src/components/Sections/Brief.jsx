77064d2a3c7233942d9f4467683d6fc25b7ad300
Aayan Ahmed <aayanahmedtejani@gmail.com>
2024-12-31 18:49:34 +0500

    dummy dtructure done

--- src/components/Sections/Brief.jsx
+++ src/components/Sections/Brief.jsx
@@ -1,21 +1,36 @@
-import React, { useEffect, useState } from 'react'
-import { MENU_API } from '../../utilis/constants';
+import React, { useEffect, useState } from "react";
+import { MENU_API } from "../../utilis/constants";
 
-const Brief = ({menu}) => {
+const Brief = ({ menu }) => {
   //* extract title and card from menu
-  const [title, card] = [menu?.[0]?.card?.card?.header.title, menu?.[0]?.card?.card?.imageGridCards.info]
+  const [title, card] = [
+    menu?.[0]?.card?.card?.header.title,
+    menu?.[0]?.card?.card?.imageGridCards.info,
+  ];
 
-  // return (
-    // <div>
-    //   <h1>{listOfRestaurant?.header?.title}</h1>
-    //   <ul>
-    //     {listOfRestaurant?.imageGridCards?.info
-    //     .map(restaurant =>
-    //       <li key={restaurant.id}>{restaurant?.action?.text}</li>
-    //     )}
-    //   </ul>
-    // </div>
-  // )
-}
+  return (
+    <div className="container">
+      <div className="cont-header">
+        <h2 className="cont-heading"></h2>
+        <div className="navigators">
+          <button className="move-right"></button>
+          <button className="move-left"></button>
+        </div>
+      </div>
 
-export default Brief
+      <div className="cont-body px-4 flex gap-6">
+        {/* through map */}
+        <div className="body-item ">
+          <a href="">
+            <div className="">
+              <img className="item-img" src="" alt="" />
+              <h4 className="item-text"></h4>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Brief;
