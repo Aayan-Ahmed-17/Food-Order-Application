5e91d56adb23e3aee2de0caab9f65c31c34c656b
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-01-01 13:48:37 +0500

    brief sec done --v.1

--- src/components/Sections/Brief.jsx
+++ src/components/Sections/Brief.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { MENU_API } from "../../utilis/constants";
+import { CDN_URL, MENU_API } from "../../utilis/constants";
 
 const Brief = ({ menu }) => {
   //* extract title and card from menu
@@ -11,22 +11,36 @@ const Brief = ({ menu }) => {
   return (
     <div className="contain h-[255px]">
       <div className="cont-header flex w-full justify-between items-center py-2 px-4">
-        <h2 className="cont-heading font-bold text-2xl tracking-tight inline-block">{title}</h2>
+        <h2 className="cont-heading font-bold text-2xl tracking-tight inline-block">
+          {title && title}
+        </h2>
         <div className="navigators">
           <button className="move-right rounded-full size-6">➡</button>
           <button className="move-left rounded-full size-6">⬅</button>
         </div>
       </div>
 
-      <div className="cont-body px-4 flex gap-6">
+      <div
+        className="cont-body px-4 flex gap-6 overflow-x-auto scroll-smooth"
+        style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
+      >
         {/* through map */}
-        <div className="body-item py-4 ">
-          <a href="">
-            <div className="img-cotainer">
-              <img className="item-img w-28 sm:w-36 aspect-[4/5]" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/12/30/7f8383da-0197-4a4a-8c52-fc6ddb9368be_CakesDesserts2.png" alt="" />
-            </div>
-          </a>
-        </div>
+        {card &&
+          card.map((e) => {
+            return (
+              <div key={e.id} className="body-item py-4 flex-shrink-0">
+                <a href={e.action.link}>
+                  <div className="img-cotainer">
+                    <img
+                      className="item-img w-28 sm:w-36 aspect-[4/5]"
+                      src={CDN_URL + e.imageId}
+                      alt={e.action.text}
+                    />
+                  </div>
+                </a>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
